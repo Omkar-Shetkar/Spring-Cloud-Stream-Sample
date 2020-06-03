50e2617e66522808c03d0e2f98bec40a6a918228
Omkar Shetkar <omkar.shetkar@outlook.com>
2020-06-03 11:30:12 +0530

    Non function supplier and multiple consumer functions.

--- src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
+++ src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
@@ -1,36 +1,58 @@
 package com.example.loggingconsumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 @SpringBootApplication
+@Controller
 public class LoggingConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoggingConsumerApplication.class, args);
     }
 
+    @Autowired
+    private StreamBridge streamBridge;
+
+    @RequestMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void delegateToSupplier(@RequestBody Person person) {
+        System.out.println("Sending " + person);
+        streamBridge.send("supplier", person);
+    }
 
     @Bean
     public Consumer<Person> consumer() {
-        return p -> System.out.println("Received: " + p);
+        return p -> System.out.println("consumer--Received: " + p);
     }
 
     @Bean
-    public Supplier<Person> supplier() {
-        return () -> {
-            Person person = new Person();
-            person.setName("Omkar");
-            System.out.println("Publishing: "+person);
-            return person;
-        };
-
+    public Consumer<Person> consumer1() {
+        return p -> System.out.println("consumer1--Received: " + p);
     }
+
+//    @Bean
+//    public Supplier<Person> supplier() {
+//        return () -> {
+//            Person person = new Person();
+//            person.setName("Omkar");
+//            System.out.println("Publishing: "+person);
+//            return person;
+//        };
+//
+//    }
 
 //    @Bean
 //    public Function<String, String> uppercase() {
